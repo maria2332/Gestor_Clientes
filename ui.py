d46024f5994ceedfc374443b7c1857cae9283e2c
maria2332 <maria.ballesteros2004@gmail.com>
2023-02-21 11:32:02 +0100

    cambio

--- ui.py
+++ ui.py
@@ -75,74 +75,74 @@ class CreateClientWindow(Toplevel, CenterWidgetMixin): #creamos la clase CreateC
             else (valor.isalpha() and len(valor) >= 2 and len(valor) <= 30) 
         event.widget.configure({"bg": "Green" if valido else "Red"})
         # Cambiar el estado del botón en base a las validaciones
-        self.validaciones[index] = valido
-        self.crear.config(state=NORMAL if self.validaciones == [1, 1, 1] else DISABLED)
+        self.validaciones[index] = valido   #establecemos el valor de la lista validaciones
+        self.crear.config(state=NORMAL if self.validaciones == [1, 1, 1] else DISABLED)  #establecemos el estado del botón Crear
 
 
-class EditClientWindow(Toplevel, CenterWidgetMixin):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.title("Actualizar cliente")
-        self.build()
-        self.center()
-        self.transient(parent)
-        self.grab_set()
+class EditClientWindow(Toplevel, CenterWidgetMixin):    #creamos la clase EditClientWindow que hereda de Toplevel y CenterWidgetMixin
+    def __init__(self, parent): #creamos el método __init__ con el atributo parent
+        super().__init__(parent)    #llamamos al método __init__ de la clase padre
+        self.title("Actualizar cliente")    #establecemos el título de la ventana
+        self.build()    #llamamos al método build
+        self.center()   #llamamos al método center
+        self.transient(parent)     #establecemos la ventana como transient
+        self.grab_set()   #establecemos la ventana como grab_set
 
-    def build(self):
-        frame = Frame(self)
-        frame.pack(padx=20, pady=10)
+    def build(self):    #creamos el método build
+        frame = Frame(self)    #creamos el frame
+        frame.pack(padx=20, pady=10)    #establecemos el padding del frame
 
-        Label(frame, text="DNI (no editable)").grid(row=0, column=0)
-        Label(frame, text="Nombre (de 2 a 30 chars)").grid(row=0, column=1)
-        Label(frame, text="Apellido (de 2 a 30 chars)").grid(row=0, column=2)
+        Label(frame, text="DNI (no editable)").grid(row=0, column=0)    #creamos el label DNI
+        Label(frame, text="Nombre (de 2 a 30 chars)").grid(row=0, column=1)   #creamos el label Nombre
+        Label(frame, text="Apellido (de 2 a 30 chars)").grid(row=0, column=2)   #creamos el label Apellido
 
-        dni = Entry(frame)
-        dni.grid(row=1, column=0)
-        nombre = Entry(frame)
-        nombre.grid(row=1, column=1)
-        nombre.bind("<KeyRelease>", lambda event: self.validate(event, 0))
-        apellido = Entry(frame)
-        apellido.grid(row=1, column=2)
-        apellido.bind("<KeyRelease>", lambda event: self.validate(event, 1))
+        dni = Entry(frame)  #creamos el entry DNI
+        dni.grid(row=1, column=0)   #establecemos la posición del entry DNI
+        nombre = Entry(frame)   #creamos el entry Nombre
+        nombre.grid(row=1, column=1)    #establecemos la posición del entry Nombre
+        nombre.bind("<KeyRelease>", lambda event: self.validate(event, 0))  #establecemos el evento KeyRelease para validar el entry Nombre
+        apellido = Entry(frame) #creamos el entry Apellido
+        apellido.grid(row=1, column=2)  #establecemos la posición del entry Apellido
+        apellido.bind("<KeyRelease>", lambda event: self.validate(event, 1))    #establecemos el evento KeyRelease para validar el entry Apellido
 
-        cliente = self.master.treeview.focus()
-        campos = self.master.treeview.item(cliente, 'values')
-        dni.insert(0, campos[0])
-        dni.config(state=DISABLED)
-        nombre.insert(0, campos[1])
-        apellido.insert(0, campos[2])
+        cliente = self.master.treeview.focus()  #establecemos el cliente
+        campos = self.master.treeview.item(cliente, 'values')   #establecemos los campos
+        dni.insert(0, campos[0])    #insertamos los datos en el entry DNI
+        dni.config(state=DISABLED)  #establecemos el estado del entry DNI como deshabilitado
+        nombre.insert(0, campos[1]) #insertamos los datos en el entry Nombre
+        apellido.insert(0, campos[2])   #insertamos los datos en el entry Apellido
+    
+        frame = Frame(self) #creamos el frame
+        frame.pack(pady=10) #establecemos el padding del frame
 
-        frame = Frame(self)
-        frame.pack(pady=10)
+        actualizar = Button(frame, text="Actualizar", command=self.edit_client) #creamos el botón Actualizar
+        actualizar.grid(row=0, column=0)    #establecemos la posición del botón Actualizar
+        Button(frame, text="Cancelar", command=self.close).grid(row=0, column=1)    #creamos el botón Cancelar
 
-        actualizar = Button(frame, text="Actualizar", command=self.edit_client)
-        actualizar.grid(row=0, column=0)
-        Button(frame, text="Cancelar", command=self.close).grid(row=0, column=1)
+        self.validaciones = [1, 1]  #creamos la lista validaciones
+        self.actualizar = actualizar    #establecemos el valor de la lista actualizar
+        self.dni = dni  #establecemos el valor de la lista dni
+        self.nombre = nombre    #establecemos el valor de la lista nombre
+        self.apellido = apellido    #establecemos el valor de la lista apellido
 
-        self.validaciones = [1, 1]
-        self.actualizar = actualizar
-        self.dni = dni
-        self.nombre = nombre
-        self.apellido = apellido
+    def edit_client(self):  #creamos el método edit_client
+        cliente = self.master.treeview.focus()      #establecemos el cliente
+        self.master.treeview.item(cliente, values=( #establecemos los valores
+            self.dni.get(), self.nombre.get(), self.apellido.get()))        
+        db.Clientes.modificar(self.dni.get(), self.nombre.get(), self.apellido.get())   #modificamos los datos en la base de datos
+        self.close()    #llamamos al método close
 
-    def edit_client(self):
-        cliente = self.master.treeview.focus()
-        self.master.treeview.item(cliente, values=(
-            self.dni.get(), self.nombre.get(), self.apellido.get()))
-        db.Clientes.modificar(self.dni.get(), self.nombre.get(), self.apellido.get())
-        self.close()
+    def close(self):    #creamos el método close
+        self.destroy()  #destruimos la ventana
+        self.update()   #actualizamos la ventana
 
-    def close(self):
-        self.destroy()
-        self.update()
-
-    def validate(self, event, index):
-        valor = event.widget.get()
-        valido = (valor.isalpha() and len(valor) >= 2 and len(valor) <= 30)
-        event.widget.configure({"bg": "Green" if valido else "Red"})
+    def validate(self, event, index):   #creamos el método validate
+        valor = event.widget.get()  #establecemos el valor del entry
+        valido = (valor.isalpha() and len(valor) >= 2 and len(valor) <= 30) #establecemos la validación
+        event.widget.configure({"bg": "Green" if valido else "Red"})    #establecemos el color del entry
         # Cambiar el estado del botón en base a las validaciones
-        self.validaciones[index] = valido
-        self.actualizar.config(state=NORMAL if self.validaciones == [1, 1] else DISABLED)
+        self.validaciones[index] = valido   #establecemos el valor de la lista validaciones
+        self.actualizar.config(state=NORMAL if self.validaciones == [1, 1] else DISABLED)   #establecemos el estado del botón Actualizar
 
 
 class MainWindow(Tk, CenterWidgetMixin):    #creamos la clase MainWindow que hereda de Tk y CenterWidgetMixin
@@ -172,42 +172,42 @@ class MainWindow(Tk, CenterWidgetMixin):    #creamos la clase MainWindow que her
         scrollbar.pack(side=RIGHT, fill=Y) #empaquetamos el scrollbar
         treeview['yscrollcommand'] = scrollbar.set  #establecemos el comando del scrollbar
 
-        for cliente in db.Clientes.lista:
-            treeview.insert(
-                parent='', index='end', iid=cliente.dni,
-                values=(cliente.dni, cliente.nombre, cliente.apellido))
+        for cliente in db.Clientes.lista:  #recorremos la lista de clientes
+            treeview.insert(    #insertamos los clientes en el treeview
+                parent='', index='end', iid=cliente.dni,    #establecemos el id del cliente como su DNI
+                values=(cliente.dni, cliente.nombre, cliente.apellido)) #establecemos los valores del cliente
 
-        treeview.pack()
+        treeview.pack() #empaquetamos el treeview
 
-        frame = Frame(self)
-        frame.pack(pady=20)
+        frame = Frame(self) #creamos el frame
+        frame.pack(pady=20) #empaquetamos el frame
 
-        Button(frame, text="Crear", command=self.create).grid(row=0, column=0)
-        Button(frame, text="Modificar", command=self.edit).grid(row=0, column=1)
-        Button(frame, text="Borrar", command=self.delete).grid(row=0, column=2)
+        Button(frame, text="Crear", command=self.create).grid(row=0, column=0)  #creamos el botón crear
+        Button(frame, text="Modificar", command=self.edit).grid(row=0, column=1)    #creamos el botón modificar
+        Button(frame, text="Borrar", command=self.delete).grid(row=0, column=2) #creamos el botón borrar
 
-        self.treeview = treeview
+        self.treeview = treeview    #establecemos el treeview como atributo de la clase
 
-    def delete(self):
-        cliente = self.treeview.focus()
+    def delete(self):   #creamos el método delete
+        cliente = self.treeview.focus() #obtenemos el cliente seleccionado
         if cliente:
-            campos = self.treeview.item(cliente, "values")
-            confirmar = askokcancel(
-                title="Confirmar borrado",
-                message=f"¿Borrar {campos[1]} {campos[2]}?",
-                icon=WARNING)
-            if confirmar:
-                self.treeview.delete(cliente)
-                db.Clientes.borrar(campos[0])
+            campos = self.treeview.item(cliente, "values") #obtenemos los valores del cliente
+            confirmar = askokcancel(    #creamos un cuadro de diálogo para confirmar el borrado
+                title="Confirmar borrado",  #establecemos el título del cuadro de diálogo
+                message=f"¿Borrar {campos[1]} {campos[2]}?",    #establecemos el mensaje del cuadro de diálogo
+                icon=WARNING)   #establecemos el icono del cuadro de diálogo
+            if confirmar:   
+                self.treeview.delete(cliente)   #borramos el cliente del treeview
+                db.Clientes.borrar(campos[0])   #borramos el cliente de la base de datos
 
-    def create(self):
-        CreateClientWindow(self)
+    def create(self):   #creamos el método create
+        CreateClientWindow(self)    #creamos una ventana para crear un cliente
 
-    def edit(self):
-        if self.treeview.focus():
-            EditClientWindow(self)
+    def edit(self): #creamos el método edit
+        if self.treeview.focus():   #comprobamos que haya un cliente seleccionado
+            EditClientWindow(self)  #creamos una ventana para editar un cliente
 
 
 if __name__ == "__main__":
-    app = MainWindow()
-    app.mainloop()
+    app = MainWindow()  #creamos la ventana principal
+    app.mainloop()  #ejecutamos el bucle principal
